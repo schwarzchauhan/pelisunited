3653bb7a375a2392faba7cf9712901063e39e8eb
Harsh Chauhan <harshchauhan0994@gmail.com>
2025-12-14 23:49:04 +0530

    Initial Commit

---
+++ pelisunited/pelisunitedApp.swift
@@ -0,0 +1,17 @@
+//
+//  pelisunitedApp.swift
+//  pelisunited
+//
+//  Created by Harsh Chauhan on 12/14/25.
+//
+
+import SwiftUI
+
+@main
+struct pelisunitedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
